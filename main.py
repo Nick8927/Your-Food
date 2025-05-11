4b131e5941dc19288630215a4cf9610c558901db
Nick <idainswan@gmail.com>
2025-05-11 22:32:55 +0300

    реализовал кнопки для сбора заказа(inline),кнопку возврата в основное меню(reply), функции для дб - получение общей цены и получение всех категорий, обработчик формирования заказа(order_handler)

--- main.py
+++ main.py
@@ -2,13 +2,14 @@ import asyncio
 from aiogram import Bot, Dispatcher
 
 from config import TOKEN
-from handlers import start, get_contact
+from handlers import start, get_contact, order_handler
 
 bot = Bot(token=TOKEN)
 dp = Dispatcher()
 
 dp.include_router(start.router)
 dp.include_router(get_contact.router)
+dp.include_router(order_handler.router)
 
 async def main():
     await dp.start_polling(bot)
