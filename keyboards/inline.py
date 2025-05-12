0ed2f846d598fccbb57869f588ae4d8c60780921
Nick <idainswan@gmail.com>
2025-05-12 15:50:42 +0300

    реализовал обработчик для показа всех продуктов конкретной категории(categories_handler), кнопку показа продуктов в категории(inline), функцию получения продуктов по category_id(utils) и добавил обработчик в бот для работы

--- keyboards/inline.py
+++ keyboards/inline.py
@@ -1,5 +1,6 @@
+from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 from aiogram.utils.keyboard import InlineKeyboardBuilder
-from database.utils import db_get_all_category, db_get_finally_price
+from database.utils import db_get_all_category, db_get_finally_price, db_get_product
 
 
 def generate_category_menu(chat_id):
@@ -22,4 +23,16 @@ def generate_category_menu(chat_id):
      for category in categories]
 
     builder.adjust(1, 2)
+    return builder.as_markup()
+
+
+def show_product_by_category(category_id: int) -> InlineKeyboardMarkup:
+    """кнопка для показа продуктов по категориям"""
+    products = db_get_product(category_id)
+    builder = InlineKeyboardBuilder()
+    [builder.button(text=product.product_name, callback_data=f'product_{product.id}') for product in products]
+    builder.adjust(2)
+    builder.row(
+        InlineKeyboardButton(text="⬅ Назад", callback_data='return_to_category')
+    )
     return builder.as_markup()
