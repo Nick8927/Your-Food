570daf28194dd032be5e86be1966d05ad8a0ea04
Nick <idainswan@gmail.com>
2025-05-12 16:08:32 +0300

    реакция на кнопку назад, при выборе товаров из категории(categories_handler) / Handled back button press when selecting products from a category (categories_handler)

--- handlers/categories_handler.py
+++ handlers/categories_handler.py
@@ -1,9 +1,10 @@
 from aiogram import Router, F
 from aiogram.types import CallbackQuery
-from keyboards.inline import show_product_by_category
+from keyboards.inline import show_product_by_category, generate_category_menu
 from aiogram.exceptions import TelegramBadRequest
 
 router = Router()
+
 
 @router.callback_query(F.data.regexp(r'^category_(\d+)$'))
 async def show_product_button(callback: CallbackQuery):
@@ -21,3 +22,17 @@ async def show_product_button(callback: CallbackQuery):
         )
     except TelegramBadRequest:
         await callback.answer("Не удалось открыть выбранную категорию")
+
+
+@router.callback_query(F.data == "return_to_category")
+async def return_to_category(callback: CallbackQuery):
+    """обработчик возврата к списку категорий"""
+    chat_id = callback.message.chat.id
+    message_id = callback.message.message_id
+
+    await callback.bot.edit_message_text(
+        text="Выберите категорию",
+        chat_id=chat_id,
+        message_id=message_id,
+        reply_markup=generate_category_menu(chat_id)
+    )
