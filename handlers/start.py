6daae073b245c56e024812904dbd669165211444
Nick <idainswan@gmail.com>
2025-05-22 23:23:32 +0300

    добавил функциональность отправки фото при старте работы с ботом(start), кнопку 'начать'

--- handlers/start.py
+++ handlers/start.py
@@ -1,30 +1,41 @@
 from aiogram import Router
-from aiogram.types import Message
+from aiogram.types import Message, FSInputFile
 from aiogram.filters import CommandStart
+from aiogram.filters import Text
 
 from database.utils import db_register_user
 from handlers.get_contact import show_main_menu
-from keyboards.reply import phone_button
+from keyboards.reply import phone_button, start_keyboard
 
 router = Router()
+
 
 @router.message(CommandStart())
 async def command_start(message: Message):
     """обработка команды start"""
-    await message.answer(
-        f"Добрый день, <i>{message.from_user.full_name}</i>\nДобро пожаловать",
-        parse_mode='HTML'
+
+    photo = FSInputFile("media/welcome.jpg")
+    await message.answer_photo(
+        photo=photo,
+        caption=f"Добрый день, <i>{message.from_user.full_name}</i>\nНажмите кнопку ниже, чтобы начать",
+        parse_mode='HTML',
+        reply_markup=start_keyboard()
     )
+
+
+@router.message(Text("🚀 Начать"))
+async def handle_start_button(message: Message):
     await register_user(message)
 
-async def register_user(message):
+
+async def register_user(message: Message):
     chat_id = message.chat.id
     full_name = message.from_user.full_name
     if db_register_user(full_name, chat_id):
         await message.answer(text=f'Вы в мире вкуснях 🍰')
         await show_main_menu(message)
     else:
-        await message.answer(text='Для связи нужен Ваш контактный номер', reply_markup=phone_button())
-
-
-
+        await message.answer(
+            text='Для связи нужен Ваш контактный номер',
+            reply_markup=phone_button()
+        )
