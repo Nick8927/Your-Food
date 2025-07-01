46df460c0d16628635bd7442411a5004a5d26197
Nick <idainswan@gmail.com>
2025-07-01 14:45:36 +0300

    фикс обработчиков отвечающих за количество товаров в корзине, перед оформление заказа(h11_cart_modify)

--- handlers/h11_cart_modify.py
+++ handlers/h11_cart_modify.py
@@ -2,10 +2,12 @@ from aiogram import Router, F
 from aiogram.types import CallbackQuery
 from aiogram.utils.keyboard import InlineKeyboardBuilder
 
+from bot_utils.cart_text import generate_cart_text
 from database.utils import (
     db_get_product_for_delete,
     db_increase_product_quantity,
-    db_decrease_product_quantity, db_get_cart_items
+    db_decrease_product_quantity,
+    db_get_cart_items
 )
 from keyboards.inline import cart_actions_keyboard
 from keyboards.reply import back_to_main_menu
@@ -15,41 +17,60 @@ router = Router()
 
 @router.callback_query(F.data == 'choose_to_add')
 async def choose_product_to_add(callback: CallbackQuery):
-    """обработчик кнопки "выбрать товар для добавления"""
+    """Обработчик кнопки "Выбрать товар для добавления"."""
     cart_products = db_get_product_for_delete(callback.from_user.id)
     builder = InlineKeyboardBuilder()
     for cart_id, name in cart_products:
         builder.button(text=f'➕ {name}', callback_data=f'increase_{cart_id}')
     builder.button(text="⬅ Назад", callback_data="back_to_cart_review")
     builder.adjust(1)
-    await callback.message.edit_text("Выберите товар для увеличения количества:", reply_markup=builder.as_markup())
+
+    await callback.message.edit_text(
+        "Выберите товар для увеличения количества:",
+        reply_markup=builder.as_markup()
+    )
     await callback.answer()
 
 
 @router.callback_query(F.data == 'choose_to_remove')
 async def choose_product_to_remove(callback: CallbackQuery):
-    """обработчик кнопки "выбрать товар для удаления"""
+    """Обработчик кнопки "Выбрать товар для удаления"."""
     cart_products = db_get_product_for_delete(callback.from_user.id)
     builder = InlineKeyboardBuilder()
     for cart_id, name in cart_products:
         builder.button(text=f'➖ {name}', callback_data=f'decrease_{cart_id}')
     builder.button(text="⬅ Назад", callback_data="back_to_cart_review")
     builder.adjust(1)
-    await callback.message.edit_text("Выберите товар для уменьшения количества:", reply_markup=builder.as_markup())
+
+    await callback.message.edit_text(
+        "Выберите товар для уменьшения количества:",
+        reply_markup=builder.as_markup()
+    )
     await callback.answer()
 
 
 @router.callback_query(F.data.startswith("increase_"))
 async def increase_quantity(callback: CallbackQuery):
-    """обработчик кнопки "увеличить количество"""
+    """Обработчик кнопки увеличения количества."""
     cart_id = int(callback.data.split("_")[1])
     db_increase_product_quantity(cart_id)
+
+    user_id = callback.from_user.id
+    cart_items = db_get_cart_items(user_id)
+    text = generate_cart_text(cart_items)
+
+    builder = InlineKeyboardBuilder()
+    builder.button(text="⬅ Назад", callback_data="back_to_cart_review")
+    builder.button(text="➕ Добавить кол-во", callback_data=f"increase_{cart_id}")
+    builder.adjust(1)
+
+    await callback.message.edit_text(text=text, reply_markup=builder.as_markup())
     await callback.answer("Количество увеличено.")
-    await choose_product_to_add(callback)
 
 
 @router.callback_query(F.data.startswith("decrease_"))
 async def decrease_quantity(callback: CallbackQuery):
+    """Обработчик кнопки уменьшения количества."""
     cart_id = int(callback.data.split("_")[1])
     db_decrease_product_quantity(cart_id)
 
@@ -62,23 +83,15 @@ async def decrease_quantity(callback: CallbackQuery):
             reply_markup=back_to_main_menu()
         )
     else:
-
-        text = "🛒 Содержимое корзины:\n\n"
-        total = 0
-        for item in cart_items:
-            subtotal = float(item.final_price)
-            total += subtotal
-            text += f"{item.product_name} — {item.quantity} шт. — {subtotal:.2f} руб\n"
-        text += f"\n💰 Итого: {total:.2f} руб"
-
+        text = generate_cart_text(cart_items)
         keyboard = cart_actions_keyboard()
         await callback.message.edit_text(text, reply_markup=keyboard)
 
-    await callback.answer()
+    await callback.answer("Количество уменьшено.")
 
 
 @router.callback_query(F.data == "back_to_cart_review")
 async def back_to_cart(callback: CallbackQuery):
-    """обработчик кнопки "назад"""
-    await callback.message.edit_text("Вот ваша корзина:", reply_markup=cart_actions_keyboard())
+    """Обработчик кнопки "Назад" к корзине."""
+    await callback.message.edit_text("Ваша корзина:", reply_markup=cart_actions_keyboard())
     await callback.answer()
