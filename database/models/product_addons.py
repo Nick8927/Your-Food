0629ddcc9391ebb0f8361b65d91a99d8fca28d0f
Nick <idainswan@gmail.com>
2025-07-05 13:16:52 +0300

    добавил новый класс модели CartAddons, для хранения выбранных добавок, добавил отношение в таблицу Carts, c классом CartAddons

--- database/models/product_addons.py
+++ database/models/product_addons.py
@@ -12,3 +12,16 @@ class ProductAddons(Base):
     product_id: Mapped[int] = mapped_column(ForeignKey("products.id", ondelete="CASCADE"))
 
     product = relationship("Products", back_populates="addons")
+
+
+class CartAddons(Base):
+    __tablename__ = "cart_addons"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    cart_id: Mapped[int] = mapped_column(ForeignKey("carts.id", ondelete="CASCADE"))
+    addon_id: Mapped[int] = mapped_column(ForeignKey("product_addons.id", ondelete="CASCADE"))
+    name: Mapped[str] = mapped_column(String(100))
+    price: Mapped[int] = mapped_column(default=0)
+
+    cart = relationship("Carts", back_populates="addons")
+    addon = relationship("ProductAddons")
