25b14094391c06323bef9b76015bd342f097d287
Nick <idainswan@gmail.com>
2025-07-22 15:04:22 +0300

    добавлены логи для регистрации пользователей, предоставлением ими ном.телефона

--- action_logger.py
+++ action_logger.py
@@ -1,13 +1,16 @@
 from logger import logger
 
-def log_user_registration(user_id: int, username: str):
-    """лог регистрацию юзера"""
-    logger.info(f"[USER_ACTION] User {user_id} (username: @{username}) registered")
 
-def log_user_order(user_id: int, order_id: int, total_price: float):
-    """лог заказ юзера"""
-    logger.info(f"[USER_ACTION] User {user_id} placed order #{order_id} for {total_price:.2f} BYN")
+def log_user_registration(username: str, phone: str | None = None):
+    """лог регистрации юзера"""
+    if phone:
+        logger.info(f"[USER_ACTION] Пользователь @{username} (тел.: {phone}) зарегистрировался")
+    else:
+        logger.info(f"[USER_ACTION] Пользователь @{username} начал регистрацию...")
 
-def log_user_deletion(user_id: int):
-    """лог удаление аккаунта юзера"""
-    logger.info(f"[USER_ACTION] User {user_id} deleted account")
+
+def log_user_phone_update(username: str, phone: str):
+    """лог добавления номера телефона"""
+    logger.info(f"[USER_ACTION] Пользователь @{username} добавил номер тел.: {phone}")
+
+
